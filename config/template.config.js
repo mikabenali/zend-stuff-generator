3b5211b9f16e12f9c366075f18b6dbac6dee369e
Ben Ali Michael <michael.benali.ext@francetvpub.fr>
2019-02-13 17:01:45 +0100

    Add rest empty template

---
+++ config/template.config.js
@@ -0,0 +1,45 @@
+export const templatesConfig = [
+  // Rest
+  {
+    type: 'rest',
+    templateFolder: 'template/rest/',
+    outputFolder: 'output/rest/',
+    templateFileName: 'collection.template.php',
+    outputFileName: 'Collection.php'
+  },
+  {
+    type: 'rest',
+    templateFolder: 'template/rest/',
+    outputFolder: 'output/rest/',
+    templateFileName: 'entity.template.php',
+    outputSuffixFileName: 'Entity.php'
+  },
+  {
+    type: 'rest',
+    templateFolder: 'template/rest/',
+    outputFolder: 'output/rest/',
+    templateFileName: 'hydrator_factory.template.php',
+    outputSuffixFileName: 'HydratorFactory.php'
+  },
+  {
+    type: 'rest',
+    templateFolder: 'template/rest/',
+    outputFolder: 'output/rest/',
+    templateFileName: 'repository.template.php',
+    outputSuffixFileName: 'Repository.php'
+  },
+  {
+    type: 'rest',
+    templateFolder: 'template/rest/',
+    outputFolder: 'output/rest/',
+    templateFileName: 'resource.template.php',
+    outputSuffixFileName: 'Resource.php'
+  },
+  {
+    type: 'rest',
+    templateFolder: 'template/rest/',
+    outputFolder: 'output/rest/',
+    templateFileName: 'resource_factory.template.php',
+    outputSuffixFileName: 'ResourceFactory.php'
+  },
+];
--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 const fs = require('fs');
 const replace = require('replace-in-file');
+import { templatesConfig } from './config/template.config';
 
-const TEXT_TO_REPLACE = 'NAME_VAR';
-
-const options = {
-  files: 'template/collection.template.php',
-  from: '/' + TEXT_TO_REPLACE + '/g',
-  to: 'Film'
-};
-
-try {
-  replace(options);
-} catch (error) {
-  console.log('error', error);
+if (process.argv.length < 3)  {
+  throw new Error('You must specifie a name');
 }
+
+const name = process.argv[2];
+const files = [
+  {type: 'rest', templateFolder: 'template/rest/', outputFolder: 'output/rest/', templateName: 'collection.template.php', suffix: 'Collection'},
+];
+
+templatesConfig.forEach(template => {
+  const source = template.templateFolder + template.templateFileName;
+  const destination = template.outputFolder + name + template.outputSuffixFileName;
+
+  fs.copyFile(source, destination, (err) => {
+    if (err) throw err;
+
+    const options = {
+      files: destination,
+      from: /NAME_VAR/g,
+      to: 'lala'
+    };
+
+    try {
+      const changes = replace.sync(options);
+    } catch (error) {
+      throw new Error(error);
+    }
+  });
+});
+
 
